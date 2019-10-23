cdb6a0e2abcb59690ce2b657aa839349cfa1ea89
Derave, Nathan <nathan.derave@accenture.com>
2019-10-23 12:26:25 +0200

    First commit

---
+++ Exo1_ReviewReader/GCF1_VisionAPI/main.py
@@ -0,0 +1,114 @@
+###############################
+# 0. Set environment variables
+###############################
+
+PROJECT_ID="MON_PROJECT_ID"
+PUB_SUB_TOPIC_NAME="MON_TOPIC"
+
+##############################
+# 1. Setup the environment
+##############################
+
+#Import libraries
+from google.cloud import storage
+from google.cloud import vision
+from google.cloud import pubsub_v1
+
+
+
+###########################################
+# CREATE HERE BELOW YOUR LIBRARIES CLIENTS
+
+## PubSub Client
+
+## Cloud Vision client
+
+###########################################
+
+
+
+#########################################################################################
+# 2. Process with OCR the images that triggered the event
+#########################################################################################
+
+"""
+Here, you will define the function that will be processed following the triggering event 
+(e.g. the upload of an image into your bucket).
+
+"""
+
+# ----------------------------------------------------
+# Process function
+def process_image(file, context):
+    """
+    Cloud Function triggered by Cloud Storage when a file is changed.
+    
+    Args:
+        file (dict): Metadata of the changed file, provided by the triggering
+                                 Cloud Storage event.
+        context (google.cloud.functions.Context): Metadata of triggering event.
+    Returns:
+        None; the output is written to stdout and Stackdriver Logging
+
+    """
+    
+    # Validate function request with parameters checkin
+    bucket = file['bucket']
+    name = file['name']
+
+    #OCR the new picture
+    detected_text=detect_text(bucket=bucket, filename=name)
+
+    ######################################################
+    #HERE, Publish to PubSub to trigger the next function
+    
+
+
+    ######################################################
+#
+# ----------------------------------------------------
+
+
+# -----------------------------------------------------------------------
+# Call the vision API to do OCR on the image
+def detect_text(bucket, filename):
+
+    """
+    
+    This function call the Vision API to apply OCR to the input image.
+
+    Args:
+        file (dict): Metadata of the changed file, provided by the triggering
+                                 Cloud Storage event.
+        context (google.cloud.functions.Context): Metadata of triggering event.
+
+    Returns:
+        The text read on the picture
+    
+    """
+    #################################################
+    # HERE, do the OCR by requesting to Vision API
+    # Make sure to explore what is contained in the resulting object
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    return rawResult
+
+    ########################################################
+    
+ 
+#
+# ----------------------------------------------------
---
+++ Exo1_ReviewReader/GCF2_TranslationNLPAPI/main.py
@@ -0,0 +1,111 @@
+###############################
+# 0. Set environment variables
+###############################
+
+TARGET_LANGUAGE="en"
+DESTINATION_BUCKET="LE_NOM_DE_VOTRE_BUCKET_DE_DESTINATION"
+
+##############################
+# 1. Setup the environment
+##############################
+
+#Import libraries
+from google.cloud import storage
+from google.cloud import translate
+from google.cloud import language
+
+from google.protobuf.json_format import MessageToDict
+
+import datetime, base64, json, os
+
+
+###########################################
+# CREATE HERE BELOW YOUR LIBRARIES CLIENTS
+
+## Translation client
+
+## NLP client
+
+## Storage client
+
+###########################################
+
+
+#########################################################################################
+# 2. Process with OCR the images that triggered the event
+#########################################################################################
+
+"""
+Here, you will define the function that will process the received PubSub message. First, use Translate API
+to retreive the text original language then translate it to the language you choose.
+
+"""
+# ----------------------------------------------------
+# PubSub reception function
+def process_text(event, context):
+    
+    """Triggered from a message on a Cloud Pub/Sub topic.
+    Args:
+         event (dict): Event payload.
+         context (google.cloud.functions.Context): Metadata for the event.
+    """
+    
+    text_to_analyze = base64.b64decode(event['data']).decode('utf-8')
+    original_image=event["attributes"]["image_name"]
+    original_image_export_name=original_image[0:-4]+".json"
+
+    #Analyze the text received with Translate API
+    result=analyzeText(text_to_analyze)
+
+    #Extract timestamp from filename
+    timestamp=extractTimestamp(original_image)
+
+    #Add timestamp and original text to result dictionnary
+    result["timestamp"]=timestamp
+    result["originalText"]=text_to_analyze
+
+    #Write result to JSON before writing to Google Cloud Storage (we have to do it)
+    with open("/tmp/myJson.json", 'w') as f:
+        json.dump(result, f)
+	
+    
+    #############################################
+    # HERE, write the JSON file to your bucket
+
+
+
+
+    ##############################################
+    
+
+#
+# ----------------------------------------------------
+
+
+# ----------------------------------------------------
+# Translate / NLP API Processing function
+def analyzeText(text):
+
+    # HERE, detect language and translate to English
+    
+    
+
+
+    # HERE, use NLP API to detect sentiment and entities
+    
+
+    # Compile everything in a dictionnary and return it
+    result={}
+    result["originalLanguage"]=detectedLanguage
+    result["translatedTextInEnglish"]=translatedText
+    result["entities"]=MessageToDict(response_entities)
+    result["sentiment"]=MessageToDict(response_sentiment)
+
+    return result
+
+
+# ----------------------------------------------------
+# Extract timestamp from filename
+def extractTimestamp(filename):
+
+    # Here extract the timestamp from the filename :-)
